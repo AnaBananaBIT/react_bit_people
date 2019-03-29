7c8874470417ea998aae02f47efd91bde56bdcc5
BIT Student <classroom@bgit.rs>
2019-03-29 15:44:23 +0100

    new

--- src/services/FetchPeople.js
+++ src/services/FetchPeople.js
@@ -8,8 +8,8 @@ const FetchPeople = (props) => {
                 <li key={obj.id.value} className="collection-item avatar">
                     <img src={obj.picture.large} alt="" className="circle"></img>
                     <span className="title"><b>{obj.name.first.charAt(0).toUpperCase()}{obj.name.first.slice(1)} {obj.name.last.charAt(0).toUpperCase()}{obj.name.last.slice(1)}</b></span>
-                    <p><i className="fas fa-envelope"></i>{obj.email}</p>
-                    <p><i className="fas fa-birthday-cake"></i>{obj.dob.date.slice(0, 10)}</p>
+                    <p><i className="fas fa-envelope">&nbsp; </i>{obj.email}</p>
+                    <p><i className="fas fa-birthday-cake">&nbsp; </i>{obj.dob.date.slice(0, 10)}</p>
                 </li>
             )
             )}
