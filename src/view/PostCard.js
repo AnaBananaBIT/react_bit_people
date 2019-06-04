8771c2c5215a9722c531c03f7ca2f6e340501976
AnaBananaBIT <anatanaskovic1210@gmail.com>
2019-06-04 18:31:06 +0200

    details

--- src/view/PostCard.js
+++ src/view/PostCard.js
@@ -1,24 +1,25 @@
 import React from 'react'
 
 const PostCard = (props) => {
-    return (      
-          props.users.map(user => (
-            < div className={`row ${user.gender}`} >
-                <div class="col 4">
-                    <div class="card teal lighten-2">
-                        <div class="card-content white-text">
-                             <span class="card-title">{user.name}</span>
-                                <img src={user.pictureLarge}/>
-                       </div>
-                         <div class="card-action">
-                             <p>{user.email}</p>
-                            <p>Birthday date:{user.dob}</p>
-                         </div>
-                        </div>
-                    </div>
-            </div >
-        ))
-
+    return (  
+          <div className="card">
+             {props.users.map(user=>( 
+      <div className="row">
+          <div className="col 12">
+          <div className="card col 4 offset-s6">
+            <div className="card-image">
+              <img src={user.pictureLarge}/>
+              <span className="card-title">{user.name}</span>
+            </div>
+            <div className="card-content">
+              <p>{user.email}</p>
+              <p>Birthday:{user.dob}</p>
+            </div>
+          </div>
+          </div>
+          </div>
+        ))}
+     </div>
     )
 }
 
